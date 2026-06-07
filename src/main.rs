9a17ed774f3e7a6d53d2fdfbc6e9d5bcc32de11c
nicolaszmarzagao <nicolas.marzagao@gmail.com>
2026-06-07 13:00:57 -0300

    read arguments

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,19 @@ fn main() {
             println!("Usage: program [api key] [target]");
         }
     }
-
+    /*
     if let Some(program_name) = args.nth_back(0) {
         println!("program name: {}", program_name);
     } else {
         println!("Not found");
+    }
+    */
+
+    args.next(); // skip program name
+    for arg in args {
+        match arg.as_str() {
+            "--help" => println!("Help Argument found!"),
+            _ => println!("None: {}", arg.as_str()),
+        }
     }
 }
