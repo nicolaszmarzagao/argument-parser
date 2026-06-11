a22124012b101181501f19c970e2b98f4e497b08
nicolaszmarzagao <nicolas.marzagao@gmail.com>
2026-06-11 19:32:27 -0300

    feat: hello clitool

--- examples/hello.c
+++ examples/hello.c
@@ -5,8 +5,9 @@ int main(int argc, char** argv)
 {
     // Set up Parser Flags
     ParserConfig parser = parser_new("hello", "0.0.1", "A simple hello commandline tool");
-    parser_add_flag(&parser, "--name", STR, true);
-    parser_add_flag(&parser, "--count", INT, false);
+
+    parser_add_flag(&parser, "--name", STR, true);   // required flag
+    parser_add_flag(&parser, "--count", INT, false); // optional flag
 
     int err = parser_parse(&parser, argc, argv);
     if (err != 0) return err;
@@ -20,12 +21,8 @@ int main(int argc, char** argv)
         count = parser_get_flag_value_int(&parser, "--count");
 
     // CLI Functionality
-    if (name)
-        for (int i=0; i<count; i++)
-            printf("Hello %s!\n", name);
-        
-    else // should be checked internally in the library
-        printf("ERROR: A name was not found\n");
+    for (int i=0; i<count; i++)
+        printf("Hello %s!\n", name);
 
     return 0;
 }
